6b880a4d8912df53aa537837b906e2117a3d2390
Thomas <thomasmckenna12gmail.com>
2021-12-20 17:29:11 -0500

    NavBar, and Home page completed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { movies } from "../data";
 
+
+
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  // const [movie, setMovies] = useState(movies)
+
+  // return (
+  // <div>{/*{code here}*/}
+  //   <h1>Movies Page</h1>
+  //   <div>{}</div>
+  // </div>
+  // )
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,55 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>{/*{code here}*/}
+  <ul> 
+    <li>
+      <NavLink
+        to="/Home"
+      >
+        Home
+      </NavLink>
+    </li>
+    <li> 
+      <NavLink
+        to="/movies"
+      >
+        Movies
+      </NavLink>
+    </li>
+    <li> 
+      <NavLink
+        to="/directors"
+      >
+        Directors
+      </NavLink>
+    </li>
+    <li> 
+      <NavLink
+        to="/actors"
+      >
+        Actors
+      </NavLink>
+    </li>
+
+
+
+  </ul>
+
+  </div>
+    ) 
 }
 
 export default NavBar;
